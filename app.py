4afefb574bb306f3f330b7779f5a4cb6b2b412a0
ThrosturH3090 <trolli2000@gmail.com>
2018-01-25 13:39:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,21 @@ from bottle import *
 
 @route('/')
 def index():
-    return "<h1>Verkefni 2</h1><a href='/about'>Liður A</a>""\n""<a href='/biography'>Liður B</a>"
+    return "<h1>Verkefni 2</h1><a href='/about'>About  </a>""<a href='/biography'>Biography</a>""<a href='/Pictures'>Pictures</a>"
 @route('/about')
 def about():
-    return "Lorem ipsum litlum fiskum"
+    return "<a href='/sida1'>Síða 1</a>""<a href='/sida2'>Síða 2</a>""<a href='/sida3'>Síða 3</a>"
 @route('/biography')
 def biography():
     return "Lorem ipsum kriddum diskum"
-
+@route('/sida1')
+def sida1():
+    return "Þetta er síða 1"
+@route('/sida2')
+def sida1():
+    return "Þetta er síða 2"
+@route('/sida3')
+def sida1():
+    return "Þetta er síða 3"
   
 run(host='0.0.0.0', port=os.environ.get('PORT'))
