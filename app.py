1c2422b59a3dc9ffde7d2e4b8cb5defde53dd198
ThrosturH3090 <trolli2000@gmail.com>
2018-01-25 14:06:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ def about():
     "<a href='/sida3'>Síða 3</a>"
 @route('/lidur2')
 def lidur2():
-    return "<a href='/sida_1'>Síða 1</a>""\n"
+    return 
+    "<a href='/sida_1'>src=<img height="140px" "myndir/1.png"></a>""\n"
     "<a href='/sida_2'>Síða 2</a>""\n"
     "<a href='/sida_3'>Síða 3</a>"
 @route('/sida1')
@@ -23,16 +24,6 @@ def sida2():
 @route('/sida3')
 def sida3():
     return "Þetta er síða 3"
-@route('/sida_1')
-def sida_1():
-    return "static/1.png"
-@route('/sida_2')
-def sida_2():
-    return "static/2.png"
-@route('/sida_2')
-def sida_2():
-    return "static/3.png"
-@route('/static/myndir')
 def server_static(myndir):
     return static_file(myndir,root="./myndir")
 run(host='0.0.0.0', port=os.environ.get('PORT'))
